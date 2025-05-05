fe70d242eb467945c3ac3186ea9da33152741032
Felipe Gava <felipegva76@gmail.com>
2025-05-05 16:54:17 -0300

    novafuncaoex13

--- funcoes.py
+++ funcoes.py
@@ -156,3 +156,20 @@ def faz_jogada(dados, categoria, dic):
         p = calcula_pontos_regra_avancada(dados)
         dic['regra_avancada'][categoria] = p[categoria]
     return dic
+
+def imprime_cartela(cartela):
+    print("Cartela de Pontos:")
+    print("-"*25)    
+    for i in range(1, 7):
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_simples'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_simples'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    for i in cartela['regra_avancada'].keys():
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_avancada'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_avancada'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    print("-"*25)
